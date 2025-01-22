35b1ab2ab619e81f155cf843697475dedb26ce81
Eric5603 <ericwanyoike123@gmail.com>
2025-01-22 11:06:34 +0300

    updated the blog component

--- app/components/Blogy.tsx
+++ app/components/Blogy.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+
 interface BlogPost {
   image: string;
   category: string;
@@ -73,21 +74,6 @@ const BlogSection: React.FC = () => {
                     {post.description}
                   </p>
                   <div className="flex items-center flex-wrap">
-                    <a href="https://www.artificialintelligence-news.com/" className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                      Learn More
-                      <svg 
-                        className="w-4 h-4 ml-2" 
-                        viewBox="0 0 24 24" 
-                        stroke="currentColor" 
-                        strokeWidth="2" 
-                        fill="none" 
-                        strokeLinecap="round" 
-                        strokeLinejoin="round"
-                      >
-                        <path d="M5 12h14"></path>
-                        <path d="M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
                     <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                       <svg 
                         className="w-4 h-4 mr-1" 
